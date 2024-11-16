222e10bfce28325e074fee2513cd39ba92fb32f6
Jhennifer Lincoln <jhenniferlincoln@gmail.com>
2024-11-16 12:30:03 -0300

    atualizando cadastro

--- index.js
+++ index.js
@@ -57,11 +57,33 @@ function cadastroEmpresaView(req, res) {
                             <label for="uf" class="form-label">UF</label>
                             <select class="form-select" id="uf" name="uf" required>
                                 <option value="">Selecione...</option>
-                                <option>SP</option>
-                                <option>RJ</option>
-                                <option>MG</option>
-                                <option>ES</option>
-                                <!-- Adicionar outros estados -->
+                                <option value="AC">AC</option>
+                                <option value="AL">AL</option>
+                                <option value="AP">AP</option>
+                                <option value="AM">AM</option>
+                                <option value="BA">BA</option>
+                                <option value="CE">CE</option>
+                                <option value="DF">DF</option>
+                                <option value="ES">ES</option>
+                                <option value="GO">GO</option>
+                                <option value="MA">MA</option>
+                                <option value="MT">MT</option>
+                                <option value="MS">MS</option>
+                                <option value="MG">MG</option>
+                                <option value="PA">PA</option>
+                                <option value="PB">PB</option>
+                                <option value="PR">PR</option>
+                                <option value="PE">PE</option>
+                                <option value="PI">PI</option>
+                                <option value="RJ">RJ</option>
+                                <option value="RN">RN</option>
+                                <option value="RS">RS</option>
+                                <option value="RO">RO</option>
+                                <option value="RR">RR</option>
+                                <option value="SC">SC</option>
+                                <option value="SP">SP</option>
+                                <option value="SE">SE</option>
+                                <option value="TO">TO</option>
                             </select>
                         </div>
                         <div class="mb-3">
@@ -82,7 +104,7 @@ function cadastroEmpresaView(req, res) {
                     <h2>Empresas Cadastradas</h2>
                     <ul>
                         ${empresas.map(empresa => `
-                            <li>${empresa.razao} - ${empresa.cnpj}</li>
+                            <li>${empresa.razao} - ${empresa.cnpj} - ${empresa.email} - ${empresa.telefone}</li>
                         `).join('')}
                     </ul>
                 </div>
